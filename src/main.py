f2dca041869435a59f319eb2ee54cac95d00e8fc
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-07-13 12:15:40 +0700

    Refactor code structure for improved readability and maintainability

---
+++ src/main.py
@@ -0,0 +1,8 @@
+"""entry point for frontend app and backend api."""
+
+# fast pi from app
+
+# streamlit from streamlit
+
+# will be called from start.bat (in windows) or start.sh (in linux)
+# so its easier to wrap both in one script
